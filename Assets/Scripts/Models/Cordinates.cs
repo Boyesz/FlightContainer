35af447289f3b6830c287c2601da4f757ddc8563
Répási Kristóf <kristof_18_@hotmail.com>
2018-07-30 10:47:58 +0200

    Change double value type to float

--- Assets/Scripts/Models/Cordinates.cs
+++ Assets/Scripts/Models/Cordinates.cs
@@ -4,12 +4,12 @@
 public class Coordinates
 {
     public string name;
-    public double x;
-    public double y;
-    public double z;
-    public double rotX;
-    public double rotY;
-    public double rotZ;
-    public double rotW;
+    public float x;
+    public float y;
+    public float z;
+    public float rotX;
+    public float rotY;
+    public float rotZ;
+    public float rotW;
 }
 
